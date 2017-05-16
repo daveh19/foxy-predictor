10bac6bd976b5685474d16097da61f233a266c6d
d069049 <oliver.atanaszov@sap.com>
2017-05-16 11:44:51 +0200

    Added growth to endpoints

--- Backend/endpoints.py
+++ Backend/endpoints.py
@@ -17,11 +17,8 @@ app = Flask(__name__)
 # Create the appropriate app.route functions,
 #test and see if they work
 
-
-#app.route() decorator here
+#Make an app.route() decorator here
 @app.route("/")
-
-#Another decorator which defines parties endpoint
 @app.route("/parties/", methods = ['GET', 'POST'])
 def partiesFunction():
   if request.method == 'GET':
@@ -30,33 +27,41 @@ def partiesFunction():
 
   elif request.method == 'POST':
     #Call the method to create a new party
-
     name = request.args.get('name', '')
     abbreviation = request.args.get('abbreviation', '')
     leader = request.args.get('leader', '')
     alliance = request.args.get('alliance', '')
     return createANewParty(name, abbreviation, leader, alliance)
 
-#Another app.route() which gets details for a specific party
-@app.route("/parties/<int:id>", methods = ['GET'])
+#Make another app.route() decorator here that takes in an integer id in the URI
+@app.route("/parties/<int:id>", methods = ['GET', 'PUT', 'DELETE'])
 #Call the method to view a specific party
 def partiesFunctionId(id):
-  if request.method == 'GET':
-    return getParty(id)
+    if request.method == 'GET':
+        return getParty(id)
 
-#Another app.route() which gets all the projections
-@app.route("/parties/projection/", methods = ['GET'])
-#Call the method to view prjojections for all parties
-def partiesFunctionProjection():
-  if request.method == 'GET':
-    return getPartyProjections()
 
-#Another app.route() which gets all the projections
-@app.route("/parties/popularity/", methods = ['GET'])
-#Call the method to view prjojections for all parties
-def partiesFunctionPopularity():
-  if request.method == 'GET':
-    return getPartyPopularity()
+@app.route("/growth", methods = ['GET', 'POST'])
+def growthFunction():
+    if request.method == 'GET':
+    #Call the method to Get all of the country growth
+        return getGrowth()
+
+    elif request.method == 'POST':
+        year = request.args.get('year', '')
+        percentage = request.args.get('percentage', '')
+        return createNewGrowth(year, growth)
+
+def createNewGrowth(year, percentage):
+    growth = Growth(year = year, percentage = percentage)
+    session.add(growth)
+    session.commit()
+    return jsonify(Growth=growth.serialize)
+
+def getGrowth():
+    growths = session.query(Growth).all()
+    return jsonify(Growth=[g.serialize for g in growths])
+
 
 # Get all parties in a list
 def getAllParties():
@@ -75,15 +80,6 @@ def getParty(id):
   party = session.query(Party).filter_by(id = id).one()
   return jsonify(party=party.serialize)
 
-# Get all party projections
-def getPartyProjections():
-  projections = session.query(Projection).all()
-  return jsonify(Projections=[i.serialize for i in projections])
-
- # Get all party projections
- def getPartyPopularity():
-     popularities = session.query(Popularity).all()
-     return jsonify(Popularities=[i.serialize for i in popularities])
 
 if __name__ == '__main__':
     app.debug = False
