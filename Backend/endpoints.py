090a9d81e4e14e1d778496210ea5dfa59ea024d5
Etjen Ymeraj <etjenymeraj@outlook.com>
2017-05-18 21:29:13 +0200

    All Endpoints added! All Get Requests added! Next step: Do Post requests!

---
+++ Backend/endpoints.py
@@ -0,0 +1,96 @@
+from flask import Flask, request, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, Party, Polling, Projection, Growth, Election, Popularity
+
+# Create an engine with the database specified
+engine = create_engine('sqlite:///polls.db')
+Base.metadata.bind = engine
+
+# Create a session for your engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+# Declare app
+app = Flask(__name__)
+
+# Initial app.route() decorator
+@app.route("/")
+
+# Polling app.route() decorator
+@app.route("/parties/polls", methods=['GET', 'POST'])
+def pollingFunction():
+    if request.method == 'GET':
+        return getPollingData()
+    elif request.method == 'POST'
+        return loadPollingData()
+
+# Parties app.route() decorator
+@app.route("/parties/", methods=['GET', 'POST'])
+def partiesFunction():
+    if request.method == 'GET':
+        return getAllParties()
+
+# Growth app.route() decorator
+@app.route("/growth", methods=['GET'])
+def growthFunction():
+    if request.method == 'GET':
+        return getGrowth()
+
+# Get parties data
+def getAllParties():
+    all_parties = session.query(Party).all()
+    parties = []
+    for party in all_parties:
+        popularity = []
+        election = []
+        party_rows = session.query(Popularity).filter(party.id == Popularity.party_id).all()
+        for p in party_rows:
+            popularity.append({
+                "state_name" : p.state_name,
+                "percentage" : p.percentage
+            })
+        election_rows = session.query(Election).filter(party.id == Election.party_id).all()
+        for e in election_rows:
+            election.append({
+                "year" : e.year,
+                "percentage" : e.percentage
+            })
+        parties.append({
+            "name" : party.name,
+            "leader": party.leader,
+            "abbreviation": party.abbreviation,
+            "popularity": popularity,
+            "past_election": election
+        })
+    return jsonify(parties)
+
+# Get growth data
+def getGrowth():
+    growths = session.query(Growth).all()
+    return jsonify(Growth=[g.serialize for g in growths])
+
+# Get polling data
+def getPollingData():
+    polling = session.query(Polling).all()
+    firms = []
+    for firm in polling:
+        parties = []
+        party_rows = session.query(Projection).filter(firm.id == Projection.polling_id).all()
+        for party in party_rows:
+            parties.append({
+                "party_name" : party.party_name,
+                "percentage" : party.percentage,
+                "people" : party.people
+            })
+        firms.append({
+            'firm': firm.firm_name,
+            'parties': parties
+        })
+
+    return jsonify(firms)
+
+# Run app
+if __name__ == '__main__':
+    app.debug = False
+    app.run(host='0.0.0.0', port=5000)
---
+++ Backend/models.py
@@ -0,0 +1,103 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, Enum, create_engine, Float, func, LargeBinary
+from sqlalchemy.ext.declarative import declarative_base
+from flask import Flask, request, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship, sessionmaker
+
+
+# Base Declaration
+Base = declarative_base()
+
+# Party Class
+class Party(Base):
+    __tablename__ = 'party'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    leader = Column(String(80), nullable=False)
+    abbreviation = Column(String(80), nullable=False)
+    elections = relationship('Election', backref = 'party', lazy='dynamic')
+
+    def serialize(self):
+        return {
+        "party_name" : self.party_name,
+        "leader": self.leader,
+        "abbreviation": self.abbreviation
+        }
+
+# Polling Class
+class Polling(Base):
+    __tablename__ = 'polling'
+    id = Column(Integer, primary_key=True)
+    firm_name = Column(String(80), nullable=False)
+    projections = relationship('Projection', backref = 'polling', lazy='dynamic')
+
+    def serialize(self):
+        return {
+        "firm_name" : self.firm_name
+        }
+
+
+# Projection Class
+class Projection(Base):
+    __tablename__ = 'projection'
+    id = Column(Integer, primary_key=True)
+    party_name = Column(String(80), nullable =False)
+    percentage = Column(Integer, nullable = False)
+    people = Column(String(80), nullable = False)
+    polling_id = Column(Integer, ForeignKey('polling.id'))
+
+    def serialize(self):
+        return {
+        "party_name" : self.party_name,
+        "percentage" : self.percentage,
+        "people" : self.people
+        }
+
+# Election Class
+class Election(Base):
+    __tablename__ = 'election'
+
+    id = Column(Integer, primary_key=True)
+    year = Column(Integer, nullable=False)
+    percentage = Column(Integer, nullable=False)
+    party_id = Column(Integer, ForeignKey('party.id'))
+
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'year': self.year,
+            'percentage': self.percentage
+        }
+
+# Popularity Class
+class Popularity(Base):
+    __tablename__ = 'popularity'
+
+    id = Column(Integer, primary_key=True)
+    state_name = Column(String(80), nullable=False)
+    percentage = Column(Integer, nullable=False)
+    party_id = Column(Integer, ForeignKey('party.id'))
+
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'state_name': self.state_name,
+            'percentage': self.percentage
+        }
+# Growth class
+class Growth(Base):
+    __tablename__ = 'growth'
+
+    id = Column(Integer, primary_key=True)
+    year = Column(Integer, nullable=False)
+    percentage = Column(Integer, nullable=False)
+
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'year': self.year,
+            'percentage': self.percentage
+            }
+
+engine = create_engine('sqlite:///polls.db')
+Base.metadata.create_all(engine)
