8dbda137ea5e326658781da58c428295c4fd4915
Oliver Atanaszov <oliver.atanaszov@gmail.com>
2017-06-13 12:48:47 +0200

    refactoring + cleanup

--- Backend/AllPartiesAPIOperations/APICalls.py
+++
@@ -1,7 +0,0 @@
-import json
-import pandas as pd
-
-def getAllParties():
-    response = pd.read_json('http://localhost:5000/parties/')
-    #jsonResponse = json.load(response.decode('utf-8'))
-    return response
