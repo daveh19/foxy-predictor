c6c38ccf62b2e18677ff3220fe31465534c7abb2
Etjen Ymeraj <etjenymeraj@outlook.com>
2017-06-13 10:11:44 +0200

    Get method for parties endpoint

--- Backend/AllPartiesAPIOperations/getAllParties.py
+++ Backend/AllPartiesAPIOperations/getAllParties.py
@@ -1,9 +1,11 @@
 import httplib2
 import json
+import pandas as pd
 
 def getAllParties():
     # FORMAT: https://localhost:5000/parties/
     h = httplib2.Http()
     url = ('http://localhost:5000/parties/')
     resp, content = h.request(url)
+    return pd.from_dict(json_dict)
     assert resp.status == 200
