814b2d62c9e8bd1f3699d08b8364b4113cf75335
Claudia Winklmayr <claudiaw@bccn-berlin.de>
2017-07-11 11:51:07 +0200

    added new model names

--- Python_Gui/vars.py
+++ Python_Gui/vars.py
@@ -6,8 +6,13 @@ DATA_PATH = DIR_PATH + '/data'# where to save data to/ read data from
 
 PARTIES = ['CDU/CSU', 'SPD', 'GRÜNE', 'FDP', 'LINKE', 'AfD', 'Sonstige']
 
-POLLING_FIRMS = ['allensbach', 'emnid', 'forsa', 'politbarometer', 'gms', 'dimap', 'insa']
+POLLING_FIRMS = ['allensbach', 'emnid', 'forsa', 'politbarometer', 'gms', 'dimap', 'insa', 'public', 'yougov', 'research', 'psephos', 'behrens']
 
-MODELS = ['----', 'AverageModel', 'WeightedAverageModel', 'LatestModel', 'WeightedLatestModel', 'DecayModel', 'WeightedDecayModel', 'LinearRegressionModel']
+#MODELS = ['----', 'AverageModel', 'WeightedAverageModel', 'LatestModel', 'WeightedLatestModel', 'DecayModel', 'WeightedDecayModel', 'LinearRegressionModel']
+
+MODELS = ['----', 'AverageModel', 'LatestModel', 'PolynomialModel', 'LinearModel', 'DecayModel', 'GPModel']
 
 HELP_TEXT = 'still no help for you....'
+
+
+
