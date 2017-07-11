a12d44b8f1f99fa6d9d43fad75f8b2edf36507d0
David Higgins (TUPro) <dave@uiginn.com>
2017-07-11 15:06:32 +0200

    Ok, there's something really weird about running 'python endpoints.py' from the load script. It uses a different codeset (problems with umlauts) and cannot use the same libraries. I think the difference is that the .bashrc is not loaded on the headless setup. Let's leave it like this for now and run it manually. (It should be safe to run from a cronjob, by the way.)

--- Docker_Server/datascience_container_generation/run_ipython.sh
+++ Docker_Server/datascience_container_generation/run_ipython.sh
@@ -5,8 +5,7 @@ sudo nginx
 
 echo 'Run python endpoints.py after loading the docker container, it wont work here'
 #echo 'Starting our database backend server, internal host 5000'
-#cd /home/ds/notebooks/Backend
-#python /home/ds/notebooks/Backend/endpoints.py &
+#(cd /home/ds/notebooks/Backend && python endpoints.py&)
 
 echo 'Starting jupyter notebook server, port 8888'
 /opt/ds/bin/jupyter-notebook --no-browser --port 8888 --ip=0.0.0.0 
