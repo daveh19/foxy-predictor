8dd57454141837a07f96230620a3f0a231587180
David Higgins (TUPro) <dave@uiginn.com>
2017-07-11 14:48:17 +0200

    Playing with Docker build and runtime settings. This is the best solution so far, you still need to run the endpoints server from the command-line after loading the container.

--- Docker_Server/datascience_container_generation/build_container.sh
+++ Docker_Server/datascience_container_generation/build_container.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 FOLDER=$1
 
--- Docker_Server/datascience_container_generation/run_ipython.sh
+++ Docker_Server/datascience_container_generation/run_ipython.sh
@@ -1,7 +1,13 @@
-#! /bin/bash
+#!/bin/bash
 
+echo 'Starting nginx webserver, port 80'
+sudo nginx
+
+echo 'Run python endpoints.py after loading the docker container, it wont work here'
+#echo 'Starting our database backend server, internal host 5000'
+#cd /home/ds/notebooks/Backend
+#python /home/ds/notebooks/Backend/endpoints.py &
+
+echo 'Starting jupyter notebook server, port 8888'
 /opt/ds/bin/jupyter-notebook --no-browser --port 8888 --ip=0.0.0.0 
 
-#sudo nginx
-
-#python /home/ds/notebooks/Backend/endpoints.py &
