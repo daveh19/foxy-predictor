0dce142a34bd5c1eefc526f8c61abe83d43a74b7
David Higgins (EC2 instance) <dave@uiginn.com>
2017-07-14 14:04:47 +0000

    Skeleton file for automated updating of db, can be called from outside of docker to update internal db.

---
+++ Backend/script_load_new_db.py
@@ -0,0 +1,17 @@
+#!/opt/ds/bin/python
+import os
+
+#os.system('source /opt/ds/bin/activate')
+print('Initial run directory')
+os.system('pwd') 
+
+os.chdir('/home/ds/notebooks/Backend')
+print('Changed run directory to')
+os.system('pwd')
+
+
+from endpoints import loadPollingData
+print('About to load new polling data from web')
+#loadPollingData()
+print('Automated script to load new polling data ended')
+
