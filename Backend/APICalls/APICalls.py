40258ad263cf808911ab85d9a76929c42f4b3445
Oliver Atanaszov <oliver.atanaszov@gmail.com>
2017-07-18 16:33:25 +0200

    inconsistency fixed in APICalls/getPollingData(states=True)

--- Backend/APICalls/APICalls.py
+++ Backend/APICalls/APICalls.py
@@ -4,13 +4,38 @@ Module handling the API endpoint calls to pull data from the database.
 getAllParties: returns party names
 getPollingData: returns a dictionary of polling data (one dataframe per firm)
 """
-import json
 import pandas as pd
+import datetime
+
 
 def getAllParties():
     response = pd.read_json('http://localhost:5000/parties/')
     #jsonResponse = json.load(response.decode('utf-8'))
     return response
+
+
+def correct_date(df):
+    '''
+    detects pollings with the same date of publication from the same polling firm and
+    changes the date of the earlier (least recent) poll to one day further in the past,
+    e.g. 2017-03-15 -> 2017-03-14
+    df:     pandas DataFrame that contains polls of one polling firm (!)
+            and the column 'date'
+    return: The same DataFrame without multiple occurence of the same publication date
+    '''
+    for i in range(len(df) - 1):
+
+        j = i + 1
+
+        if df.iloc[i]['date'] == df.iloc[j]['date']:
+            s = df.iloc[j]['date'].split('-')
+            new_day = int(s[-1]) - 1
+            s_new = s
+            s_new[-1] = "%02d" %new_day
+            new_date = '-'.join(s_new)
+            df.date.iloc[j] = new_date
+
+    return df
 
 def getPollingData(state = False):
     """
@@ -18,15 +43,36 @@ def getPollingData(state = False):
     :param state: If True, returns state data. Else returns federal data. By default False.
 
     """
-    response = pd.read_json('http://127.0.0.1:5000/parties/polls')
+    response = pd.read_json('http://localhost:5000/parties/polls')
 
     df_per_firm = {}
     for i in range(len(response)):
                 r = response.ix[[i]]
                 df = pd.DataFrame.from_records(r.get('parties')[i])
-                df_p = df.pivot_table(index=['date'], columns='party_name', values='percentage')
-                date_to_people = {k: v for k, v in zip(df['date'], df['people'])}
-                date_to_region = {k: v for k, v in zip(df['date'], df['region'])}
+                if state:
+                    df = df[df.region != 'germany'].copy()
+
+                    regions = list(set(df.region))
+                    print(regions)
+
+                    df_corr = []
+                    for reg in regions:
+                        df_reg = df[df.region == reg].copy()
+                        df_corr.append(correct_date(df_reg))
+                    df_corr = pd.concat(df_corr, ignore_index=True)
+                    date_to_people = {k: v for k, v in zip(df_corr['date'], df_corr['people'])}
+                    date_to_region = {k: v for k, v in zip(df_corr['date'], df_corr['region'])}
+
+                else:
+
+                    df_ger = df[df.region == 'germany'].copy()
+                    df_corr = correct_date(df_ger)
+                    date_to_people = {k: v for k, v in zip(df_corr['date'], df_corr['people'])}
+                    date_to_region = {k: v for k, v in zip(df_corr['date'], df_corr['region'])}
+
+                df_p = df_corr.pivot_table(index=['date'], columns=['party_name'], values='percentage')
+                print(len(df_p))
+
                 ppl = []
                 reg = []
                 for ind in df_p.index:
@@ -35,9 +81,6 @@ def getPollingData(state = False):
                 df_p['region'] = reg
                 df_p['people'] = ppl
                 df_p = df_p.sort_index(ascending=False)
-                if state:
-                    df_per_firm[r['firm'][i]] = df_p[df_p['region'] != 'germany']
-                else:
-                    df_per_firm[r['firm'][i]] = df_p[df_p['region'] == 'germany']
-                # f_per_firm[r['firm'][i]] = df_p
+                df_per_firm[r['firm'][i]] = df_p
+
     return df_per_firm
