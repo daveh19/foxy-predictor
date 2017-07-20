33cb8b5dcbdeba2f9b76744a0ac9c2e182968531
EC2 Default User <ec2-user@ip-172-31-27-204.us-west-2.compute.internal>
2017-07-20 10:56:27 +0000

    Updated the daily script to ensure no EOF error when parsing to the gui

--- Docker_Server/datascience_container_generation/Foxy_daily_run.sh
+++ Docker_Server/datascience_container_generation/Foxy_daily_run.sh
@@ -1,4 +1,5 @@
 cd /home/ds/notebooks/Python_Gui
-echo D no y y y y y y y y y y y | python fox_gui.py 2> /var/log/$(date +%Y-%m-%d_%H:%M).log
+#echo D no y y y y y y y y y y y | python fox_gui.py 2> /var/log/$(date +%Y-%m-%d_%H:%M).log
+cat input |python fox_gui.py 2> /var/log/$(date +%Y-%m-%d_%H:%M).log
 cp Dashboard.html ../html/`date +\%Y\%m\%d`.html
 rm Dashboard.html
