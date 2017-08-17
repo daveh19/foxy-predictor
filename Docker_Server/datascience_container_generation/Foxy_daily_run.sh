499648a357966c5f9cd4ce2d27da0031e6df4cc3
root <root@ip-172-31-27-204.us-west-2.compute.internal>
2017-08-17 09:40:03 +0000

    Updated Docker File

--- Docker_Server/datascience_container_generation/Foxy_daily_run.sh
+++ Docker_Server/datascience_container_generation/Foxy_daily_run.sh
@@ -5,14 +5,23 @@ PATH=/opt/ds/bin:/usr/bin:/bin:/sbin; export PATH
 #echo D no y y y y y y y y y y y | python fox_gui.py 2> /var/log/$(date +%Y-%m-%d_%H:%M).log
 #cat cron_input_bayes | python /home/ds/notebooks/Python_Gui/fox_gui.py | sed -e "s/^/$(date -R) /" 2> /tmp/BayesModel.log
 date >> /tmp/BayesModel.log
-cat cron_input_bayes | python /home/ds/notebooks/Python_Gui/fox_gui.py 2> /tmp/BayesModel.log
 #cat cron_input_bayes | python /home/ds/notebooks/Python_Gui/fox_gui.py 2> /tmp/BayesModel.log
-cp Dashboard.html ../html/`date +\%Y\%m\%d`-BayesModel.html
-rm Dashboard.html
+#cat cron_input_bayes | python /home/ds/notebooks/Python_Gui/fox_gui.py 2> /tmp/BayesModel.log
+#cp Dashboard.html ../html/`date +\%Y\%m\%d`-BayesModel.html
+#rm Dashboard.html
 #cat cron_input_gpflow | python /home/ds/notebooks/Python_Gui/fox_gui.py 2> /tmp/GPFlowModel.log
 #cp Dashboard.html ../html/`date +\%Y\%m\%d`-GPFlowModel.html
 #rm Dashboard.html
+date >> /tmp/DecayModel.log
 cat cron_input_decay | python /home/ds/notebooks/Python_Gui/fox_gui.py 2> /tmp/DecayModel.log
 cp Dashboard.html ../html/`date +\%Y\%m\%d`-DecayModel.html
+rm Dashboard.html
+date >> /tmp/LatestModel.log
+cat cron_input_latest | python /home/ds/notebooks/Python_Gui/fox_gui.py 2> /tmp/LatestModel.log
+cp Dashboard.html ../html/`date +\%Y\%m\%d`-LatestModel.html
+rm Dashboard.html
+date >> /tmp/GPModel.log
+cat cron_input_gpflow | python /home/ds/notebooks/Python_Gui/fox_gui.py 2> /tmp/GPModel.log
+cp Dashboard.html ../html/`date +\%Y\%m\%d`-GPModel.html
 rm Dashboard.html
 exit 0
